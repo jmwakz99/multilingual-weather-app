435557924da90e420aea2a465e8d3e611f2d6144
jmwakz99 <kemsjunior99@gmail.com>
2024-04-25 17:46:30 +0200

    update github pages url

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  return <div className='App'>Hello world!</div>;
 }
 
 export default App;

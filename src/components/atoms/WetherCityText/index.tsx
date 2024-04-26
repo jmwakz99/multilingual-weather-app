5b9973531c0856290320302e53f93a095f452e6e
jmwakz99 <kemsjunior99@gmail.com>
2024-04-26 14:41:10 +0200

    add weather city text atom

---
+++ src/components/atoms/WetherCityText/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './index.module.css';
+
+const WeatherCityText = () => {
+  return <h1 className={classes['weather-city']}>index</h1>;
+};
+
+export default WeatherCityText;

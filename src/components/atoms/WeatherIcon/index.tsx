2567a98d7d0d26e861560b16418989bf15fbf7a9
jmwakz99 <kemsjunior99@gmail.com>
2024-04-26 15:00:51 +0200

    add weather icon

---
+++ src/components/atoms/WeatherIcon/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './index.module.css';
+
+const WeatherIcon: React.FC = () => {
+  return <div className={classes['weather-icon']}>Icon</div>;
+};
+
+export default WeatherIcon;

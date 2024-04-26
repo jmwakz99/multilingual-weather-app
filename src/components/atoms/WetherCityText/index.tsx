953f49aad31ba0a345a0c633e02aa18875b88647
jmwakz99 <kemsjunior99@gmail.com>
2024-04-26 14:41:58 +0200

    add weather city text atom

--- src/components/atoms/WetherCityText/index.tsx
+++ src/components/atoms/WetherCityText/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './index.module.css';
 
 const WeatherCityText = () => {
-  return <h1 className={classes['weather-city']}>index</h1>;
+  return <h1 className={classes['weather-city']}>Nairobi, Kenya</h1>;
 };
 
 export default WeatherCityText;
